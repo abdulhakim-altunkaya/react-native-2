92819fc3c79d2b3a9b27d02f7d51e0340c4acc8a
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-23 19:26:03 +0300

    list display is compeleted with many key options

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListItems from "./src/screens/ListItems";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListItems,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App",
     },
